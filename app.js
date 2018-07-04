0700aa7c38acb040fe1c7550b274485b034a200d
Trixi Turny <trixi.turny@weswap.com>
2018-07-04 11:34:04 +0100

    Get started with basic functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+const pdfInvoice = require('pdf-invoice');
+const document = pdfInvoice({
+    company: {
+      phone: '(99) 9 9999-9999',
+      email: 'company@evilcorp.com',
+      address: 'Av. Companhia, 182, Água Branca, Piauí',
+      name: 'Evil Corp.',
+    },
+    customer: {
+      name: 'Elliot Raque',
+      email: 'raque@gmail.com',
+    },
+    items: [
+      {amount: 50.0, name: 'XYZ', description: 'Lorem ipsum dollor sit amet', quantity: 12},
+      {amount: 12.0, name: 'ABC', description: 'Lorem ipsum dollor sit amet', quantity: 12},
+      {amount: 127.72, name: 'DFE', description: 'Lorem ipsum dollor sit amet', quantity: 12},
+    ],
+  });
+ 
+const fs = require('fs');
+document.generate(); // triggers rendering
+document.pdfkitDoc.pipe(fs.createWriteStream('./test.pdf'));
