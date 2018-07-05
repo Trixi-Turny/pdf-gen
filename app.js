079fdc1d72415a154e8bdf733c76e2da7b7c3fca
Trixi Turny <trixi.turny@weswap.com>
2018-07-05 13:53:53 +0100

    customise width of columns

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const pdfInvoice = require('pdf-invoice');
 const document = pdfInvoice({
     company: {
       phone: '(99) 9 9999-9999',
-      email: 'company@evilcorp.com',
+      email: 'company@weswap.com',
       address: 'Av. Companhia, 182, Água Branca, Piauí',
-      name: 'Evil Corp.',
+      name: 'WeSwap',
     },
     customer: {
       name: 'Elliot Raque',
       email: 'raque@gmail.com',
     },
     items: [
-      {amount: 50.0, name: 'XYZ', description: 'Lorem ipsum dollor sit amet', quantity: 12},
-      {amount: 12.0, name: 'ABC', description: 'Lorem ipsum dollor sit amet', quantity: 12},
-      {amount: 127.72, name: 'DFE', description: 'Lorem ipsum dollor sit amet', quantity: 12},
+      {date:'2018-03-02', description: 'Lorem ipsum dollor sit ametLorem ipsum dollor sit amet', local_amount: 10, fx_rate: 34.5,  amount: 15 },
+      {date:'2018-03-02', description: 'Lorem ipsum dollor sit amet', local_amount: 100, fx_rate: 34.5,  amount: 100 },
+      {date:'2018-03-02', description: 'Lorem ipsum dollor sit amet', local_amount: 1873, fx_rate: 34.5,  amount: 1550 },
     ],
   });
  
 const fs = require('fs');
 document.generate(); // triggers rendering
-document.pdfkitDoc.pipe(fs.createWriteStream('./test.pdf'));
+document.pdfkitDoc.pipe(fs.createWriteStream('./testerr.pdf'));
